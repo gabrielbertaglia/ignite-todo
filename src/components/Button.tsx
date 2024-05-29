4eb4e8b4e3cc54b31049699eb5f51ec05df35c1f
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-05-29 10:12:30 -0300

    fix: props button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 import { PlusCircle } from "@phosphor-icons/react";
 import styles from "./Button.module.css";
+import { ButtonHTMLAttributes } from "react";
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function Button({ children }: ButtonProps) {
+export function Button({ children, ...props }: ButtonProps) {
   return (
-    <button className={styles.button}>
+    <button className={styles.button} {...props}>
       {children}&ensp;
       <PlusCircle weight="bold" size={16} />
     </button>

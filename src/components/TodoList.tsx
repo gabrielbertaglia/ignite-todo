68e3f6a2c4797fe0c928fa6d74cc502201027c65
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-05-28 23:58:02 -0300

    feat: creating todolist component

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,34 @@
+import { Check, Trash } from "@phosphor-icons/react";
+import styles from "./TodoList.module.css";
+import { TasksProps } from "../App";
+
+interface TodoListProps {
+  task: TasksProps;
+}
+
+export function TodoList({ task }: TodoListProps) {
+  const checkboxCheckedClassname = task.isChecked
+    ? styles["checkbox-checked"]
+    : styles["checkbox-unchecked"];
+  const paragraphCheckedClassname = task.isChecked
+    ? styles["paragraph-checked"]
+    : "";
+
+  return (
+    <div className={styles.container}>
+      <label htmlFor="checkbox">
+        <input readOnly type="checkbox" checked={task.isChecked} />
+        <span className={`${styles.checkbox} ${checkboxCheckedClassname}`}>
+          {task.isChecked && (
+            <Check weight="bold" className={styles.icon} size={12} />
+          )}
+        </span>
+
+        <p className={`${styles.paragraph} ${paragraphCheckedClassname}`}>
+          {task.title}
+        </p>
+      </label>
+      <Trash className={styles.trash} />
+    </div>
+  );
+}

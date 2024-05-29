467bd341e7a101665f5d453d07f8f2fa13dea697
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-05-29 00:03:04 -0300

    refactor: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,56 @@
 import styles from "./App.module.css";
 import "./global.css";
-import { PlusCircle } from "@phosphor-icons/react";
+
+import { v4 as uuidv4 } from "uuid";
+
 import { Header } from "./components/Header";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import { ListHeader } from "./components/ListHeader";
+import { Empty } from "./components/Empty";
+import { TodoList } from "./components/TodoList";
+
+export interface TasksProps {
+  id: string;
+  title: string;
+  isChecked: boolean;
+}
+
+const tasks: TasksProps[] = [
+  {
+    id: uuidv4(),
+    title: "Terminar desafio",
+    isChecked: false,
+  },
+  {
+    id: uuidv4(),
+    title: "Terminar desafio",
+    isChecked: true,
+  },
+];
 
 function App() {
+  // const [tasks, setTasks] = useState<TasksProps[]>([]);
+
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
         <div className={styles.content}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button className="">
-            Criar&ensp;
-            <PlusCircle weight="bold" size={16} />
-          </button>
+          <Input placeholder="Adicione uma nova tarefa" />
+          <Button>Criar</Button>
+        </div>
+        <div className={styles.todo}>
+          <ListHeader tasks={tasks} />
+          {tasks.length > 0 ? (
+            <div className={styles.todoContainerWithTask}>
+              {tasks.map((task) => (
+                <TodoList task={task} />
+              ))}
+            </div>
+          ) : (
+            <Empty />
+          )}
         </div>
       </div>
     </>
